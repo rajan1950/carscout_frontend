6ceb7120090fa9d2ea5269efc4fe2ec4565dd06a
rajan1950 <rajandobariya6@gmail.com>
2026-03-15 00:27:33 +0530

    feat: add missing components to admin index export

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -3,6 +3,11 @@ import { AdminDashboard } from "./AdminDashboard"
 import { AdminUsers } from "./AdminUsers"
 import { AdminCars } from "./AdminCars"
 import { AdminInquiries } from "./AdminInquiries"
+import { AdminMessages } from "./AdminMessages"
+import { AdminReviews } from "./AdminReviews"
+import { AdminTestDrives } from "./AdminTestDrives"
+import { AdminSettings } from "./AdminSettings"
+import { AdminDefaultRoute } from "./AdminDefaultRoute"
 
 export { 
          AdminSidebar, 
@@ -10,4 +15,9 @@ export {
          AdminUsers,
          AdminCars,
          AdminInquiries,
+         AdminMessages,
+         AdminReviews,
+         AdminTestDrives,
+         AdminSettings,
+         AdminDefaultRoute,
       }
