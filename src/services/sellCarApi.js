3662ab8ddbeb0db30e038de83d829539ed44a78d
rajan1950 <rajandobariya6@gmail.com>
2026-03-20 13:53:36 +0530

    feat: implement sellCarApi service for adding car listings

---
+++ src/services/sellCarApi.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { readAuthSession } from "../utils/auth";
+
+const SELL_CAR_URL = "http://localhost:4444/car/add";
+
+const getAuthHeaders = () => {
+  const token = readAuthSession()?.token;
+
+  if (!token) {
+    return {};
+  }
+
+  return {
+    Authorization: `Bearer ${token}`,
+  };
+};
+
+export const sellCarApi = async (payload) => {
+  const response = await axios.post(SELL_CAR_URL, payload, {
+    headers: {
+      "Content-Type": "application/json",
+      ...getAuthHeaders(),
+    },
+  });
+
+  return response.data;
+};
