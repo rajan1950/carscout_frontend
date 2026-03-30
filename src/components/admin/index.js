98aca50bd1a5d3eff51c1e0f12808ce521be97d4
rajan1950 <rajandobariya6@gmail.com>
2026-03-30 10:32:35 +0530

    fix: update import path for AdminSidebar component

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,4 +1,4 @@
-import { AdminSidebar } from "./AdminSidebar"
+import { AdminSidebar } from "../../layouts/AdminSidebar"
 import { AdminDashboard } from "./AdminDashboard"
 import { AdminUsers } from "./AdminUsers"
 import { AdminCars } from "./AdminCars"
